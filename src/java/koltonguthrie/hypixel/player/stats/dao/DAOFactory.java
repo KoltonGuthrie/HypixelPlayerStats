a0a40a86dd4a47654fb10fcdb6d4f05ea13549f3
KoltonGuthrie <officialkolton@gmail.com>
2024-02-10 18:35:58 -0600

    First commit

---
+++ src/java/koltonguthrie/hypixel/player/stats/dao/DAOFactory.java
@@ -0,0 +1,59 @@
+package koltonguthrie.hypixel.player.stats.dao;
+
+import java.sql.Connection;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public final class DAOFactory {
+
+    private static final String PREFIX = "hypixel_stats";
+    private static final String PROPERTY_ENVCONTEXT = "envcontext";
+    private static final String PROPERTY_INITCONTEXT = "initcontext";
+
+    private Connection conn;
+
+    public DAOFactory() {
+
+        DAOProperties properties = new DAOProperties(PREFIX);
+
+        String envcontext = properties.getProperty(PROPERTY_ENVCONTEXT);
+        String initcontext = properties.getProperty(PROPERTY_INITCONTEXT);
+
+        try {
+            Context envContext = new InitialContext();
+            Context initContext = (Context) envContext.lookup(envcontext);
+            DataSource ds = (DataSource) initContext.lookup(initcontext);
+            if (ds != null) {
+                this.conn = ds.getConnection();
+            }
+        } catch (Exception e) {
+            conn = null;
+            e.printStackTrace();
+        }
+
+    }
+
+    Connection getConnection() {
+        return conn;
+    }
+
+    public boolean isClosed() {
+
+        boolean isClosed = true;
+
+        try {
+            isClosed = conn.isClosed();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return isClosed;
+
+    }
+
+    public PlayerDAO getPlayer() {
+        return new PlayerDAO(this);
+    }
+
+}
---
+++ src/java/koltonguthrie/hypixel/player/stats/dao/PlayerDAO.java
@@ -0,0 +1,251 @@
+package koltonguthrie.hypixel.player.stats.dao;
+
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.HashMap;
+
+public class PlayerDAO {
+
+    private final DAOFactory daoFactory;
+
+    final String QUERY_INSERT_PLAYER = "INSERT INTO Players (uuid) VALUES (?);";
+    final String QUERY_FIND_PLAYER = "SELECT * FROM Players WHERE ( (? IS NULL OR id = ? ) AND ( ? IS NULL OR uuid = ? ) );";
+    final String QUERY_LIST_PLAYERS = "SELECT * FROM Players;";
+
+    PlayerDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+
+    public JsonObject create(HashMap<String, Object> map) {
+        JsonObject json = new JsonObject();
+        json.put("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        json.put("success", false);
+        json.put("message", "An unhandled error occurred.");
+
+        if (!map.containsKey("uuid")) {
+            json.put("status", HttpServletResponse.SC_BAD_REQUEST);
+            json.put("message", "Bad request.");
+            return json;
+        }
+
+        Connection conn = daoFactory.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            final JsonObject p = (JsonObject) find(map).get("player");
+
+            if (p != null) {
+                json.put("success", true);
+                json.put("status", HttpServletResponse.SC_OK);
+                json.put("message", "Player with that UUID already exists.");
+                json.put("player", p);
+
+                return json;
+            }
+
+            ps = conn.prepareStatement(QUERY_INSERT_PLAYER, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, (String) map.get("uuid"));
+
+            ps.executeUpdate();
+
+            rs = ps.getGeneratedKeys();
+
+            json.put("success", true);
+            json.put("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            json.put("message", "Failed to created player.");
+
+            Integer id = null;
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            if (id != null) {
+
+                HashMap<String, Object> hm = new HashMap<>();
+                hm.put("id", id);
+
+                json.put("status", HttpServletResponse.SC_CREATED);
+                json.put("message", "Created player.");
+                json.put("player", find(hm).get("player"));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return json;
+    }
+
+    public JsonObject find(HashMap<String, Object> map) {
+        JsonObject json = new JsonObject();
+        json.put("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        json.put("success", false);
+        json.put("message", "An unhandled error occurred.");
+
+        if (!map.containsKey("uuid") && !map.containsKey("id")) {
+            json.put("status", HttpServletResponse.SC_BAD_REQUEST);
+            json.put("message", "Bad request.");
+            return json;
+        }
+
+        Connection conn = daoFactory.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            ps = conn.prepareStatement(QUERY_FIND_PLAYER);
+
+            if (map.containsKey("id")) {
+                ps.setInt(1, (Integer) map.get("id"));
+                ps.setInt(2, (Integer) map.get("id"));
+            } else {
+                ps.setNull(1, Types.INTEGER);
+                ps.setNull(2, Types.INTEGER);
+            }
+
+            if (map.containsKey("uuid")) {
+                ps.setString(3, (String) map.get("uuid"));
+                ps.setString(4, (String) map.get("uuid"));
+            } else {
+                ps.setNull(3, Types.VARCHAR);
+                ps.setNull(4, Types.VARCHAR);
+            }
+
+            if (ps.execute()) {
+                rs = ps.getResultSet();
+
+                json.put("success", true);
+                json.put("status", HttpServletResponse.SC_OK);
+                json.put("message", "Unknown Player.");
+
+                if (rs.next()) {
+                    JsonObject player = new JsonObject();
+                    player.put("id", rs.getInt("id"));
+                    player.put("uuid", rs.getString("uuid"));
+                    player.put("updated", rs.getString("updated"));
+
+                    json.put("status", HttpServletResponse.SC_OK);
+                    json.put("message", "Found player.");
+                    json.put("player", player);
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return json;
+    }
+
+    public JsonObject list() {
+        JsonObject json = new JsonObject();
+        json.put("status", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        json.put("success", false);
+        json.put("message", "An unhandled error occurred.");
+
+        Connection conn = daoFactory.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            ps = conn.prepareStatement(QUERY_LIST_PLAYERS);
+
+            if (ps.execute()) {
+                rs = ps.getResultSet();
+
+                json.put("success", true);
+                json.put("status", HttpServletResponse.SC_OK);
+                json.put("message", "No players.");
+
+                JsonArray players = new JsonArray();
+
+                int count = 0;
+                if (rs.next()) {
+                    JsonObject player = new JsonObject();
+                    player.put("id", rs.getInt("id"));
+                    player.put("uuid", rs.getString("uuid"));
+                    player.put("updated", rs.getString("updated"));
+
+                    json.put("status", HttpServletResponse.SC_OK);
+                    json.put("message", "Found players.");
+
+                    players.add(player);
+                    count++;
+                }
+
+                json.put("players", players);
+                json.put("count", count);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                    rs = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                    ps = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return json;
+    }
+}
