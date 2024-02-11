5c6d9f1d82637a865c997c923e4935f09e059c25
KoltonGuthrie <officialkolton@gmail.com>
2024-02-10 18:45:15 -0600

    Added StatDAO

---
+++ src/java/koltonguthrie/hypixel/player/stats/dao/StatDAO.java
@@ -0,0 +1,11 @@
+package koltonguthrie.hypixel.player.stats.dao;
+
+public class StatDAO {
+    
+    private final DAOFactory daoFactory;
+
+    StatDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
+}
