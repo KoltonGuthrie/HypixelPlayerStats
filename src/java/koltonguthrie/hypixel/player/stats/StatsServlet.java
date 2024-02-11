134a87edc7e74614f31d5e4a3f2f69cd32adb2bb
KoltonGuthrie <officialkolton@gmail.com>
2024-02-10 19:01:26 -0600

    Added statsServlet

---
+++ src/java/koltonguthrie/hypixel/player/stats/StatsServlet.java
@@ -0,0 +1,95 @@
+package koltonguthrie.hypixel.player.stats;
+
+import com.github.cliftonlabs.json_simple.Jsoner;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import koltonguthrie.hypixel.player.stats.dao.DAOFactory;
+
+public class StatsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        DAOFactory daoFactory = getDAOFactory(request);
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        try ( PrintWriter out = response.getWriter()) {
+            
+            HashMap<String, Object> map = new HashMap<>();
+            
+            if(request.getParameter("uuid") != null) map.put("uuid", request.getParameter("uuid"));
+            if(request.getParameter("gamemode") != null) map.put("gamemode", request.getParameter("gamemode"));
+            
+            out.write(Jsoner.serialize(daoFactory.getStats().list(map)));
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        DAOFactory daoFactory = getDAOFactory(request);
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        try ( PrintWriter out = response.getWriter()) {
+
+            HashMap<String, Object> map = getParameters(request);
+            
+            out.write(Jsoner.serialize(daoFactory.getStats().create(map)));
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private DAOFactory getDAOFactory(HttpServletRequest r) {
+        
+        DAOFactory daoFactory = null;
+        ServletContext context = r.getServletContext();
+
+        if (context.getAttribute("daoFactory") == null) {
+            System.err.println("*** Creating New DAOFactory Instance ...");
+            daoFactory = new DAOFactory();
+            context.setAttribute("daoFactory", daoFactory);
+        }
+        else {
+            daoFactory = (DAOFactory) context.getAttribute("daoFactory");
+        }
+        
+        return daoFactory;
+        
+    }
+    
+    private HashMap<String, Object> getParameters(HttpServletRequest request) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String p = URLDecoder.decode(br.readLine().trim(), Charset.defaultCharset());
+        HashMap<String , Object> parameters = new HashMap<>();
+        String[] pairs = p.trim().split("&");
+        System.out.println(p);
+        for (int i = 0; i < pairs.length; ++i) {
+            String[] pair = pairs[i].split("=");
+            parameters.put(pair[0], pair[1]);
+        }
+        return parameters;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
