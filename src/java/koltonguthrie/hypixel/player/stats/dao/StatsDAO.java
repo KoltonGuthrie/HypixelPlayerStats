06dd9e815ce62d1cb8810b89536fb69be6f1e029
KoltonGuthrie <officialkolton@gmail.com>
2024-02-10 19:03:20 -0600

    Created list and create methods

--- src/java/koltonguthrie/hypixel/player/stats/dao/StatsDAO.java
+++ src/java/koltonguthrie/hypixel/player/stats/dao/StatsDAO.java
@@ -1,11 +1,22 @@
 package koltonguthrie.hypixel.player.stats.dao;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+import java.util.HashMap;
+
 public class StatsDAO {
     
     private final DAOFactory daoFactory;
 
     StatsDAO(DAOFactory daoFactory) {
         this.daoFactory = daoFactory;
+    }
+
+    public JsonObject list(HashMap<String, Object> map) {
+        return new JsonObject();
+    }
+
+    public JsonObject create(HashMap<String, Object> map) {
+        return new JsonObject();
     }
     
 }
